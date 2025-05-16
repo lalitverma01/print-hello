2dbd022215d3a2b101f7caca4b952391cc1cbf92
lalitverma01 <lalitverma2923@gmail.comm>
2025-05-16 11:12:00 +0530

    refactor back-to-top button functionality and update styles for improved visibility

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-const btn = $('#button');
+const btn = $('back-to-top');
 $(window).on("scroll", () => btn.toggleClass("show", $(window).scrollTop() > 300));
 btn.on("click", (e) => { e.preventDefault(); $("html, body").animate({ scrollTop: 0 }, 300); });
