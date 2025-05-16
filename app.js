7cdd8cda5eff3ecb06ca2f3d5c5bf2c9745074f2
lalitverma01 <lalitverma2923@gmail.comm>
2025-05-16 11:16:11 +0530

    fix: update back-to-top button selector for improved functionality

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-const btn = $('back-to-top');
+const btn = $('#button');
 $(window).on("scroll", () => btn.toggleClass("show", $(window).scrollTop() > 300));
 btn.on("click", (e) => { e.preventDefault(); $("html, body").animate({ scrollTop: 0 }, 300); });
